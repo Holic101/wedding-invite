6199f9d04b0b07f98e81de0d4eb2d81775d2cc0c
Holic101 <dietrich.kriworotow@gmail.com>
2016-07-22 17:28:57 +0200

    final changes

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -52,11 +52,10 @@ function smoothScroll (duration) {
 
 function initialize() {
 //Google Map
-  var myCenter = new google.maps.LatLng(50.943110, 6.898520);
   var rathaus = new google.maps.LatLng(50.938472, 6.958566);
-  var lessingStrasse = new google.maps.LatLng(50.942083, 6.839572);
+  //var lessingStrasse = new google.maps.LatLng(50.942083, 6.839572);
   var mapProp = {
-    center:myCenter,
+    center:rathaus,
     //ToDo: media-query for small screens with less zoom
     zoom:12,
     mapTypeId:google.maps.MapTypeId.ROADMAP,
@@ -64,14 +63,6 @@ function initialize() {
   };
   var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 
-
-	/*var marker = new google.maps.Marker({
-	position:myCenter
-	});*/
-  var marker2 = new google.maps.Marker({
-    position: lessingStrasse,
-    clickable: true
-  });
   var marker3 = new google.maps.Marker({
     position: rathaus,
     clickable: true
@@ -79,20 +70,15 @@ function initialize() {
 
 
 	var contentString = "Rathausplatz, 50667 Köln --> Hier wird geheiratet";
-  var contentString2 = "Lessingstr. 62, 50858 Köln --> Hier wird gefeiert!"
 
 	var infowindow = new google.maps.InfoWindow({
     content: contentString
   });
 
-  var infowindow2 = new google.maps.InfoWindow({
-    content: contentString2
-  })
-
 	marker3.setMap(map);
-  marker2.setMap(map);
+
 	infowindow.open(map, marker3);
-  infowindow2.open(map, marker2);
+
 
 //Accordion
 	$('.accordion').on('click','.accordion-control',function(e){
